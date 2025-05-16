254ec5a2ffe335b2bc974bd16f42aa3f21c0e8f8
Cesaret-Code <cesaretmurseliyev@gmail.com>
2025-05-16 23:15:32 +0400

    MakeUsingError

--- Restoran/obj/Debug/net8.0/Restoran.AssemblyInfo.cs
+++ Restoran/obj/Debug/net8.0/Restoran.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Restoran")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd9674a7efc49df2141b61b25c85920f95a108e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Restoran")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Restoran")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
